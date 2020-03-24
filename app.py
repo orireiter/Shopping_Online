4814b6faf6893b0756d12e25648475930fe0169b
orireiter <60241083+orireiter@users.noreply.github.com>
2020-03-24 15:37:24 +0200

    patcjes2

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
 from flask import Flask, redirect, render_template, request
-import db_connect
+import db_for_home
 
 app = Flask(__name__)
 
+# -----------------------------------------------------------#
 
 @app.route("/")
 def get_home():
     return redirect("/login")
 
+# default route, not used here
+# -----------------------------------------------------------#
 
 @app.route("/<username>/<page_num>")
 def home(username, page_num):
     
-    get_40 = db_connect.show_40(page_num=page_num)
+    get_40 = db_for_home.show_40(page_num=page_num)
 
     if get_40 == "exception":
         return redirect("/"+username+"/1")
     
     else:    
-        return render_template("home.html", username=username, get_40=get_40)
+        next1 = "/"+str(username)+"/"+str(int(page_num)+1)
+        back1 = "/"+str(username)+"/"+str(int(page_num)-1)
+        print(next1)
+        return render_template("home.html",
+         username=username, get_40=get_40, page_num=page_num, next1=next1,
+         back1=back1)
+
+# central part, need to add search bar
+# need to make the shopping bag and ability to add to it
+# jumping to certain page
+# ability to choose how much of each object to add
+# check out option
+# -----------------------------------------------------------#
 
 @app.route("/login", methods=['POST','GET'])
 def login():
@@ -37,7 +52,10 @@ def login():
         print("nothing")
     return render_template("login.html")
 
-
+# the login page, requires work, need to add the users to db 
+# and retrieve their pass to validate, and not let passing through
+# straight to the shopping part
+# -----------------------------------------------------------#
 
 if __name__ == "__main__":
     app.run(debug=True)
